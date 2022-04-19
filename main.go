1292d1bab27f0b345e77e1e7cbe3b4e3535ad932
Rushi Panchariya <rushipanchariya19@gmail.com>
2022-04-19 18:09:37 +0530

    flags and logic changes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -15,8 +16,12 @@ import (
 
 var log *logrus.Logger
 
-func init() {
-	logLevel, err := logrus.ParseLevel(os.Getenv("LOG_LEVEL"))
+func main() {
+	log_level := flag.String("log-level", "info", "To set log level like debug,info...")
+	serverPort := flag.Int("server-port", 9092, "Server serve apis on given port")
+	flag.Parse()
+
+	logLevel, err := logrus.ParseLevel(*log_level)
 	if err != nil {
 		logLevel = logrus.InfoLevel
 	}
@@ -29,17 +34,14 @@ func init() {
 		Level: logLevel,
 	}
 
-}
-
-func main() {
 	user := data.NewUserDetail(*log)
 
 	// create a new gRPC server, use WithInsecure to allow http connections
 	grpcServer := grpc.NewServer()
-
+	log.Debugf("gRPC server is initalized")
 	// create an instance of the Users Server
 	u := server.NewUsers(user, *log)
-
+	log.Debugf("Users is initalized")
 	// register the Users detail server
 	protos.RegisterUserDetailServer(grpcServer, u)
 
@@ -48,13 +50,13 @@ func main() {
 	reflection.Register(grpcServer)
 
 	// create a TCP socket for inbound server connections
-	l, err := net.Listen("tcp", fmt.Sprintf(":%d", 9092))
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", *serverPort))
 	if err != nil {
 		log.Error("Unable to create listener", "error", err)
 		os.Exit(1)
 	}
 
 	// listen for requests
-	log.Infof("Starting gRPC server on port: 9092")
+	log.Infof("Starting gRPC server on port: %d", *serverPort)
 	grpcServer.Serve(l)
 }
